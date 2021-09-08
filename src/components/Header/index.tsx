620c3258dd1b78e42ff90cdbbb3ba6581452ebdc
Gui-dev <adrianon10@gmail.com>
2021-09-08 01:47:15 -0300

    feat: add property onOpenNewTransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { Container, Content } from './style'
 import logoImg from './../../assets/images/logo.svg'
 
-export const Header = () => {
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void
+}
+
+export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="cifrão com o nome dtmoney do lado" title="Money" />
-        <button>Nova transação</button>
+        <button
+          onClick={ onOpenNewTransactionModal }
+        >
+          Nova transação
+        </button>
+
       </Content>
     </Container>
   )
