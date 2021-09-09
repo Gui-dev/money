3421d19b67bbc7f1aea36c1468e63ef8ac26025c
Gui-dev <adrianon10@gmail.com>
2021-09-09 16:05:56 -0300

    feat: add hook useTransaction

---
+++ src/hooks/useTransaction.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+
+import { TransactionsContext } from './../contexts/TransactionsContext'
+
+export const useTransaction = () => {
+  const context = useContext(TransactionsContext)
+
+  return context
+}
