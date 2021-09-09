a9ac4ec5f7cd97ed165835ce4b810a741bb4930a
Gui-dev <adrianon10@gmail.com>
2021-09-09 16:25:05 -0300

    feat: add hook useTransaction and transaction summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,9 +3,28 @@ import outcomeImg from './../../assets/images/outcome.svg'
 import totalImg from './../../assets/images/total.svg'
 import { Container } from './style'
 
-// import { useTransaction } from './../../hooks/useTransaction'
+import { useTransaction } from './../../hooks/useTransaction'
+import { formatNumber } from '../../utils/formatValues'
 
 export const Summary = () => {
+  const { transactions } = useTransaction()
+
+  const summary = transactions.reduce((accumulator, transaction) => {
+    if (transaction.type === 'deposit') {
+      accumulator.deposits += transaction.amount
+      accumulator.total += transaction.amount
+    } else {
+      accumulator.withdraws += transaction.amount
+      accumulator.total -= transaction.amount
+    }
+
+    return accumulator
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
+
   return (
     <Container>
       <section>
@@ -13,7 +32,7 @@ export const Summary = () => {
           <h1>Entradas</h1>
           <img src={incomeImg} alt="Uma seta apontando para cima" title="Entradas" />
         </header>
-        <strong>R$1.000,00</strong>
+        <strong>{ formatNumber(summary.deposits) }</strong>
       </section>
 
       <section>
@@ -21,7 +40,7 @@ export const Summary = () => {
           <h1>Saídas</h1>
           <img src={outcomeImg} alt="Uma seta apontando para baixo" title="Saídas" />
         </header>
-        <strong>- R$1.000,00</strong>
+        <strong>- { formatNumber(summary.withdraws) }</strong>
       </section>
 
       <section className="highlight-background">
@@ -29,7 +48,7 @@ export const Summary = () => {
           <h1>Total</h1>
           <img src={totalImg} alt="O símbolo de cifrão" title="Total" />
         </header>
-        <strong>R$1.000,00</strong>
+        <strong>{ formatNumber(summary.total) }</strong>
       </section>
     </Container>
   )
